0aaa624c012d5472f7e9157a19ac3dc185aa59a3
Gabrieli Martins de Almeida <gabrieli.almeida@maistodos.com>
2022-03-15 02:03:01 -0300

    first commit

---
+++ src/screens/Theme/index.ts
@@ -0,0 +1,13 @@
+export default {
+  COLORS: {
+    BACKGROUN: '#f2f2f2',
+    TEXT: '#170126',
+    ALERT: '#73074D',
+
+    PRIMARY_900: '#280540',
+    PRIMARY_800: '#3B0C59',
+  },
+  FONTS: {
+
+  }
+};
